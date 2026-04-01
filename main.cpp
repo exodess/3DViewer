77834b259b12638bb3b8f448e425439781ee32b9
Georgiy <goshakovalev5@gmail.com>
2026-04-01 19:40:43 +0300

    Main file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QApplication>
+#include <QSurfaceFormat>
+#include "ui/mainwindow.h"
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    
+    QSurfaceFormat format;
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setDepthBufferSize(24);
+    QSurfaceFormat::setDefaultFormat(format);
+    
+    s21::MainWindow window;
+    window.show();
+    
+    return app.exec();
+}
